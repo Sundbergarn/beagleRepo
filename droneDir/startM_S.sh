165ee4669e9a9e3a9cc8cfc22fd540da3ed74011
root <root@beaglebone.localdomain>
2022-09-10 14:37:28 +0200

    Removed practice folder

--- droneDir/startM_S.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-i2cset -y 2 0x68 0x6B 0x00
-
-config-pin p2.28 pruout
-config-pin p2.30 pruout
-config-pin p2.32 pruout
-config-pin p2.34 pruout
--- droneDir/tst.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-
-void main(){
-    float a = 3.8;
-    printf("Integer: %d\n",  (int) round(a));
-    
-}
